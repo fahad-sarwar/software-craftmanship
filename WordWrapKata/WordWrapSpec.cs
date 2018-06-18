77981e8c1407ee5e3eccb9d7f1f46ad2327ab18e
fahad-sarwar <mohammed_fahad00@hotmail.com>
2018-06-18 23:18:15 +0100

    + Updated WordWrap kata

--- WordWrapKata/WordWrapSpec.cs
+++ WordWrapKata/WordWrapSpec.cs
@@ -1,5 +1,5 @@
-using System;
 using System.Collections.Generic;
+using System.Linq;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace WordWrapKata
@@ -9,33 +9,60 @@ namespace WordWrapKata
     {
         [TestMethod]
         [DataRow("The", "The")]
+        [DataRow("The boy", "The boy")]
         [DataRow("The boy stood", "The boy\nstood")]
-        public void WrapText(string text, string result) => Assert.AreEqual(result, string.Join("\n", new WordWrapper().Process(text)));
+        [DataRow("The boy stood on the burning deck", "The boy\nstood on\nthe\nburning\ndeck")]
+        [DataRow("pneumonoultramicroscopicsilicovolcanoconiosis pneumonoultramicroscopicsilicovolcanoconiosis", "pneumonoultramicroscopicsilicovolcanoconiosis\npneumonoultramicroscopicsilicovolcanoconiosis")]
+        public void WrapText(string text, string result) => Assert.AreEqual(result, string.Join("\n", new WordWrapper(8).Process(text)));
     }
 
     public class WordWrapper
     {
+        private readonly int _threshold;
+        private readonly List<string> _result;
+
+        public WordWrapper(int threshold)
+        {
+            _threshold = threshold;
+            _result = new List<string>();
+        }
+
         public List<string> Process(string text)
         {
-            var result = new List<string>();
-
             var words = text.Split(" ");
 
-            //for (var count = 0; count < words.Length; count++)
-            //{
+            foreach (var word in words)
+            {
+                if (IsMoreThanThreshold($"{GetLastItem().Value} {word}"))
+                {
+                    AddNewItem(word);
+                    continue;
+                }
 
-            //    if ($"{temp} {words[count]}".Length > 8)
-            //    {
-            //        result.Add(temp);
-            //        temp = string.Empty;
-            //    }
-            //    else
-            //    {
-            //        temp = $"{temp} {word}";
-            //    }
-            //}
+                UpdateLastItem(word);
+            }
 
-            return result;
+            return _result;
+        }
+
+        private bool IsMoreThanThreshold(string value) => value.Trim().Length > _threshold;
+
+        private KeyValuePair<int, string> GetLastItem() => _result
+            .Select((value, index) => new KeyValuePair<int, string>(index, value))
+            .OrderByDescending(kvp => kvp.Key)
+            .DefaultIfEmpty(new KeyValuePair<int, string>(0, string.Empty))
+            .FirstOrDefault();
+
+        private void AddNewItem(string value) => _result.Add(value.Trim());
+
+        private void UpdateLastItem(string value)
+        {
+            var lastItem = GetLastItem();
+
+            if (lastItem.Equals(new KeyValuePair<int, string>(0, string.Empty)))
+                _result.Add(value.Trim());
+            else
+                _result[lastItem.Key] = $"{lastItem.Value} {value}";
         }
     }
 }
