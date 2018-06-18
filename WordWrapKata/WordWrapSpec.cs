03d6e7aee3625a32da20097cf2810024d126e522
Fahad Sarwar <fahad.sarwar@zeninternet.co.uk>
2018-06-18 18:57:46 +0100

    + Created specs

--- WordWrapKata/WordWrapSpec.cs
+++ WordWrapKata/WordWrapSpec.cs
@@ -1,3 +1,5 @@
+using System;
+using System.Collections.Generic;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace WordWrapKata
@@ -7,9 +9,33 @@ namespace WordWrapKata
     {
         [TestMethod]
         [DataRow("The", "The")]
-        public void WrapText(string text, string result)
+        [DataRow("The boy stood", "The boy\nstood")]
+        public void WrapText(string text, string result) => Assert.AreEqual(result, string.Join("\n", new WordWrapper().Process(text)));
+    }
+
+    public class WordWrapper
+    {
+        public List<string> Process(string text)
         {
-            Assert.AreEqual(result, text);
+            var result = new List<string>();
+
+            var words = text.Split(" ");
+
+            //for (var count = 0; count < words.Length; count++)
+            //{
+
+            //    if ($"{temp} {words[count]}".Length > 8)
+            //    {
+            //        result.Add(temp);
+            //        temp = string.Empty;
+            //    }
+            //    else
+            //    {
+            //        temp = $"{temp} {word}";
+            //    }
+            //}
+
+            return result;
         }
     }
 }
