788057bbebbe2b35d8b13a05727c8ff35a3da6ed
Fahad Sarwar <fahad.sarwar@zeninternet.co.uk>
2018-06-18 18:43:39 +0100

    + Created wordwrap kata

---
+++ WordWrapKata/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace WordWrapKata
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
