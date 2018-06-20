49f89e02e7e2fbac026c15ab35a58115e8a81b1b
Fahad Sarwar <fahad.sarwar@zeninternet.co.uk>
2018-06-20 13:06:31 +0100

    + Created RomanNumeral kata

---
+++ RomanNumeralsKata/RomanNumeralsSpec.cs
@@ -0,0 +1,66 @@
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace RomanNumeralsKata
+{
+    [TestClass]
+    public class RomanNumeralsSpec
+    {
+        [TestMethod]
+        [DataRow(0, "")]
+        [DataRow(1, "I")]
+        [DataRow(2, "II")]
+        [DataRow(3, "III")]
+        [DataRow(4, "IV")]
+        [DataRow(5, "V")]
+        [DataRow(6, "VI")]
+        [DataRow(7, "VII")]
+        [DataRow(8, "VIII")]
+        [DataRow(9, "IX")]
+        [DataRow(10, "X")]
+        [DataRow(11, "XI")]
+        [DataRow(14, "XIV")]
+        [DataRow(15, "XV")]
+        [DataRow(16, "XVI")]
+        [DataRow(20, "XX")]
+        [DataRow(25, "XXV")]
+        [DataRow(49, "XLIX")]
+        [DataRow(99, "XCIX")]
+        [DataRow(100, "C")]
+        public void RomanNumeralsConverter(int number, string romanNumeral) =>
+            Assert.AreEqual(romanNumeral, new RomanNumeralConverter().Convert(number));
+    }
+
+    public class RomanNumeralConverter
+    {
+        private readonly Dictionary<int, string> _lookup = new Dictionary<int, string>
+        {
+            //{ 1000, "M"},
+            //{ 900, "CM"},
+            //{ 500, "D"},
+            //{ 400, "CD"},
+            { 100, "C"},
+            { 90, "XC"},
+            { 50, "L"},
+            { 40, "XL"},
+            { 10, "X"},
+            { 9, "IX"},
+            { 5, "V"},
+            { 4, "IV"},
+            { 1, "I" },
+            { 0, "" }
+        };
+
+        public string Convert(int number)
+        {
+            if (number == 0) return string.Empty;
+
+            var result = LookupConversion(number);
+
+            return LookupConversion(number).Value + Convert(number - result.Key);
+        }
+
+        private KeyValuePair<int, string> LookupConversion(int number) => _lookup.First(n => n.Key <= number);
+    }
+}
