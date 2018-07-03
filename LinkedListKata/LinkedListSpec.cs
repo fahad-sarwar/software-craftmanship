da9e0ff56368156bdb81aa6b7a8e4b577344c66c
Fahad Sarwar <fahad.sarwar@zeninternet.co.uk>
2018-07-03 18:37:35 +0100

    + Created linkedlist kata spec

---
+++ LinkedListKata/LinkedListSpec.cs
@@ -0,0 +1,136 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace LinkedListKata
+{
+    public class LinkedListSpec
+    {
+        // Test-drive your own implementation of a linked list, WITHOUT using arrays or any standard libarary classes.
+        
+        // It should be possible to:
+        // - Find the index of any object in the list
+        // - Add/remove objects at any point in the list
+        // - Directly access any objects in the list by giving its index
+
+        // For extra bonus points:
+        // - Use generics to allow the list to contain any type of object
+        // - Implement your solution without using conditionals
+        // - Allow the list to be searched and filtered using some kind of matcher on the objects it contains
+
+        [TestClass]
+        public class WhenTheLinkedListIsEmpty
+        {
+            [TestMethod]
+            public void The_item_count_is_empty()
+            {
+            }
+
+            //[TestMethod]
+            //public void The_item_count_is_zero()
+            //{
+            //}
+
+            //[TestMethod]
+            //[ExpectedException(typeof(IndexOutOfRangeException))]
+            //public void Retrieving_an_item_by_index_results_in_an_exception()
+            //{
+            //}
+
+        }
+
+        [TestClass]
+        public class WhenTheLinkedListHasOneItem
+        {
+            //[TestMethod]
+            //public void The_item_count_is_one()
+            //{
+            //}
+
+            //[TestMethod]
+            //public void The_item_can_be_retrieved_by_its_index()
+            //{
+            //}
+
+            //[TestMethod]
+            //public void The_index_can_be_retrieved_for_the_item()
+            //{
+            //}
+
+            //[TestMethod]
+            //public void The_list_is_not_empty()
+            //{
+            //}
+        }
+
+        [TestClass]
+        public class WhenTheLinkedListHasMultipleItems
+        {
+            //[TestMethod]
+            //public void The_item_count_reflects_this()
+            //{
+            //}
+
+            //[TestMethod]
+            //public void The_items_can_be_retrieved_by_their_indexes()
+            //{
+            //}
+
+            //[TestMethod]
+            //public void The_indexes_can_be_retrieved_for_the_items()
+            //{
+            //}
+
+            //[TestMethod]
+            //public void Can_add_an_item_to_the_start_of_the_list()
+            //{
+            //}
+
+            //[TestMethod]
+            //public void Can_add_an_item_to_the_end_of_the_list()
+            //{
+            //}
+
+            //[TestMethod]
+            //public void Can_add_an_item_to_the_middle_of_the_list()
+            //{
+            //}
+
+            //[TestMethod]
+            //public void The_list_is_not_empty()
+            //{
+            //}
+        }
+
+        [TestClass]
+        public class TheLinkedListSupportsMultipleTypes
+        {
+            //[TestMethod]
+            //public void Int_is_supported()
+            //{
+            //}
+
+            //[TestMethod]
+            //public void String_is_supported()
+            //{
+            //}
+
+            //[TestMethod]
+            //public void Complex_types_are_supported()
+            //{
+            //}
+
+            //public class DummyClass
+            //{
+            //    private readonly int _id;
+            //    private readonly string _name;
+
+            //    public DummyClass(int id, string name)
+            //    {
+            //        _id = id;
+            //        _name = name;
+            //    }
+
+            //    public override bool Equals(object obj) => obj is DummyClass other && other._id == _id && other._name == _name;
+            //}
+        }
+    }
+}
