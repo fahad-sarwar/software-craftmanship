64dd91718054c70770def67dc9980b1d33890975
Fahad Sarwar <fahad.sarwar@zeninternet.co.uk>
2018-07-05 14:57:54 +0100

    + Created MarsRover kata placeholder

---
+++ MarsRoverKata/MarsRoverSpec.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace MarsRoverKata
+{
+    [TestClass]
+    public class MarsRoverSpec
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
